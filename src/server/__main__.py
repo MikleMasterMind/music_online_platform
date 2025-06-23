1ef6879ea830db95673de74959f833737465bf89
Mihail <mihuil_00@mail.ru>
2025-06-23 21:12:12 +0300

    add mysql in docker

--- src/server/__main__.py
+++ src/server/__main__.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         port=1337,
         db_host='localhost',
         db_user='music_user',
-        db_password=input("Enter the password to connect to the database: "),
+        db_password="your_strong_password",
         db_name='music_db',
         db_auth_plugin='mysql_native_password')
     asyncio.run(server.start())
